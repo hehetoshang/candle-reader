6b3c8026cab6f5f744396c296c0a95807f9454cb
hehetoshang <houheya@outlook.com>
2026-04-17 01:45:37 +0000

    Revert commits 6ca9cf4..6ca9cf4
    This reverts 1 commit(s) from 6ca9cf4 to 6ca9cf4.
    
    Reverted commits:
    6ca9cf4 feat: 分析项目
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/components/EpubReader.vue
+++ src/components/EpubReader.vue
@@ -146,6 +146,7 @@ import BookToc from './BookToc.vue'
 import Guest from './Guest.vue'
 import UserCenter from './UserCenter.vue'
 import BookComments from './BookComments.vue'
+import { EdgeSpeechTTS } from '@lobehub/tts'
 
 export default {
   name: 'EpubReader',
